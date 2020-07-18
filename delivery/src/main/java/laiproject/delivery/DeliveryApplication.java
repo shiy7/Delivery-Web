3d9e43cfa8327e1647bda2aeac0c7cd0412c9bc8
Soup.Tang <tangyunzhi916@gmail.com>
2020-07-17 22:23:25 -0500

    Google api implement

--- delivery/src/main/java/laiproject/delivery/DeliveryApplication.java
+++ delivery/src/main/java/laiproject/delivery/DeliveryApplication.java
@@ -1,5 +1,10 @@
 package laiproject.delivery;
 
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DistanceMatrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,5 +23,29 @@ public class DeliveryApplication {
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
+
+    @GetMapping("/recommend")
+    public String recommend() {
+//        final GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyCxiKSD-acPm1syrHWVQtCln60p1QTuoQM");
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyCxiKSD-acPm1syrHWVQtCln60p1QTuoQM")
+                .build();
+        String userAddress = "1031 Irving St, San Francisco, CA 94122";
+        String receiverAddress = "450 10th St, San Francisco, CA 94103";
+
+        try{
+            DistanceMatrixApiRequest req = DistanceMatrixApi.newRequest(context);
+            DistanceMatrix trix = req.origins(userAddress).destinations(receiverAddress).await();
+
+            return trix.toString();
+        }catch (ApiException e){
+            System.out.println(e.getMessage());
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+
 
 }
