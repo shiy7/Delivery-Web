338dc051c422d7e66648711ae49b20614d98fb00
Soup.Tang <tangyunzhi916@gmail.com>
2020-07-19 21:57:50 -0500

    fix CORS problem

--- Front-end/delivery-web/src/components/Recommend.js
+++ Front-end/delivery-web/src/components/Recommend.js
@@ -3,14 +3,15 @@ import {API_ROOT} from "../constants"
 
 class Recommend extends Component {
     state = {
-        response: null
+        data: null
     }
 
 
     getResponse = ( ) => {
-        const url = '/recommend';
+        // const url = 'https://cors-anywhere.herokuapp.com/https://stackoverflow.com/questions/53998407/getting-undefined-when-trying-to-fetch-data-from-an-api-react';
         // const url = 'http://localhost:8080/recommend';
         // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = '/recommend'
 
         fetch( url, {
             method: 'GET',
