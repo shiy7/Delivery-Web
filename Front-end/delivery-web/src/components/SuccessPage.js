9484e354b91cfcb9d6e10fe0ed982a2cb28e89a4
Soup.Tang <tangyunzhi916@gmail.com>
2020-07-26 23:30:15 -0500

    alter ui of successpage

--- Front-end/delivery-web/src/components/SuccessPage.js
+++ Front-end/delivery-web/src/components/SuccessPage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Button, Card, Col, Radio, Row} from "antd";
+import {Button, Result} from "antd";
 import {withRouter} from "react-router-dom";
 
 class SuccessPage extends Component {
@@ -10,13 +10,20 @@ goDashBoard = () => {
 }
     render() {
         return (
-            <div>
 
+            <Result
+                status="success"
+                title='Success! Your package is on the way!'
+                subTitle=''
+                extra={[
+                    <p>Success! Your package is on the way! Your tracking number is {localStorage.getItem("tracking")}</p>,
+                    <Button type="primary" key="dashboard" onClick={this.goDashBoard}>
+                        Back to dashboard
+                    </Button>
+                    // <Button key="buy">Buy Again</Button>,
+                ]}
+            />
 
-                <p>Success! Your package is on the way! Your tracking number is {localStorage.getItem("tracking")}</p>
-                <Button onClick={this.goDashBoard}>Back to dashboard</Button>
-                {/*<Button>New order</Button>*/}
-            </div>
         );
     }
 }
